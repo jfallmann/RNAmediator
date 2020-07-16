37438f0a5b84fabd61de93bbf1dc6a63cc40558e
jfallmann <fall@bioinf.uni-leipzig.de>
2020-07-16 17:19:08 +0200

    logging fix

--- RIssmed/ConstraintPLFold.py
+++ RIssmed/ConstraintPLFold.py
@@ -8,9 +8,9 @@
 ## Created: Thu Sep  6 09:02:18 2018 (+0200)
 ## Version:
 ## Package-Requires: ()
-## Last-Updated: Thu Jul 16 17:13:28 2020 (+0200)
+## Last-Updated: Thu Jul 16 17:18:59 2020 (+0200)
 ##           By: Joerg Fallmann
-##     Update #: 428
+##     Update #: 430
 ## URL:
 ## Doc URL:
 ## Keywords:
@@ -94,18 +94,27 @@ import datetime
 from lib.logger import makelogdir, setup_multiprocess_logger, checklog
 
 scriptname = os.path.basename(__file__).replace('.py','')
-if not (checklog()):
-    print('CREATING LOG FILES')
-    makelogdir('LOGS')
-    logfile = 'LOGS/'+scriptname+'.log'
-    if not os.path.isfile(os.path.abspath(logfile)):
-            open(logfile,'a').close()
-    else:
-        ts = str(datetime.datetime.fromtimestamp(os.path.getmtime(os.path.abspath(logfile))).strftime("%Y%m%d_%H_%M_%S"))
-        shutil.copy2(logfile,'LOGS/'+scriptname+'_'+ts+'.log')
+try:
+    if not (checklog()):
+        print('CREATING LOG FILES',file=sys.stderr)
+        makelogdir('LOGS')
+        logfile = 'LOGS/'+scriptname+'.log'
+        if not os.path.isfile(os.path.abspath(logfile)):
+                open(logfile,'a').close()
+        else:
+            ts = str(datetime.datetime.fromtimestamp(os.path.getmtime(os.path.abspath(logfile))).strftime("%Y%m%d_%H_%M_%S"))
+            shutil.copy2(logfile,'LOGS/'+scriptname+'_'+ts+'.log')
 
-    log = setup_multiprocess_logger(log_file='stderr', logformat='%(asctime)s %(levelname)-8s %(name)-12s %(message)s', datefmt='%m-%d %H:%M')
-    log = setup_multiprocess_logger(log_file=logfile, filemode='a', logformat='%(asctime)s %(levelname)-8s %(name)-12s %(message)s', datefmt='%m-%d %H:%M')
+        log = setup_multiprocess_logger(log_file='stderr', logformat='%(asctime)s %(levelname)-8s %(name)-12s %(message)s', datefmt='%m-%d %H:%M')
+        log = setup_multiprocess_logger(log_file=logfile, filemode='a', logformat='%(asctime)s %(levelname)-8s %(name)-12s %(message)s', datefmt='%m-%d %H:%M')
+
+except Exception as err:
+    exc_type, exc_value, exc_tb = sys.exc_info()
+    tbe = tb.TracebackException(
+        exc_type, exc_value, exc_tb,
+    )
+    print(''.join(tbe.format()),file=sys.stderr)
+
 
 ##load own modules
 from lib.Collection import *
