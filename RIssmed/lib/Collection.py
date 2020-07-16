1bcf031e8edf34bc40227f638025468b4b25ce4f
jfallmann <fall@bioinf.uni-leipzig.de>
2020-07-16 17:11:34 +0200

    logging fix

--- RIssmed/lib/Collection.py
+++ RIssmed/lib/Collection.py
@@ -7,9 +7,9 @@
 ## Created: Thu Sep  6 09:02:18 2018 (+0200)
 ## Version:
 ## Package-Requires: ()
-## Last-Updated: Thu Jul 16 17:10:32 2020 (+0200)
+## Last-Updated: Thu Jul 16 17:11:22 2020 (+0200)
 ##           By: Joerg Fallmann
-##     Update #: 368
+##     Update #: 371
 ## URL:
 ## Doc URL:
 ## Keywords:
@@ -88,8 +88,7 @@ from Bio.Seq import Seq
 try:
     scriptn = os.path.basename(inspect.stack()[-1].filename).replace('.py','')
     #scriptn = os.path.basename(__file__).replace('.py','')
-    log = logging.getLogger(__name__.scriptn)
-
+    log = logging.getLogger()
 
 except Exception as err:
     exc_type, exc_value, exc_tb = sys.exc_info()
