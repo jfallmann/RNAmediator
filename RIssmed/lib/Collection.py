3299f8c5d01e32b6d9a05a701389b319ba4eef85
jfallmann <fall@bioinf.uni-leipzig.de>
2020-07-17 10:21:39 +0200

    import

--- RIssmed/lib/Collection.py
+++ RIssmed/lib/Collection.py
@@ -7,9 +7,9 @@
 ## Created: Thu Sep  6 09:02:18 2018 (+0200)
 ## Version:
 ## Package-Requires: ()
-## Last-Updated: Fri Jul 17 10:20:29 2020 (+0200)
+## Last-Updated: Fri Jul 17 10:21:29 2020 (+0200)
 ##           By: Joerg Fallmann
-##     Update #: 374
+##     Update #: 375
 ## URL:
 ## Doc URL:
 ## Keywords:
@@ -61,7 +61,7 @@
 ### Code:
 ### IMPORTS
 import os, sys, inspect
-import .logger
+import lib.logger
 ##other modules
 import numpy as np
 import heapq
