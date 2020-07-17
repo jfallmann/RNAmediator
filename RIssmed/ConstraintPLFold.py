d64334662d519886afa152f46fc6137e0ecee3e0
jfallmann <fall@bioinf.uni-leipzig.de>
2020-07-17 10:17:01 +0200

    share log

--- RIssmed/ConstraintPLFold.py
+++ RIssmed/ConstraintPLFold.py
@@ -8,9 +8,9 @@
 ## Created: Thu Sep  6 09:02:18 2018 (+0200)
 ## Version:
 ## Package-Requires: ()
-## Last-Updated: Fri Jul 17 10:10:53 2020 (+0200)
+## Last-Updated: Fri Jul 17 10:16:43 2020 (+0200)
 ##           By: Joerg Fallmann
-##     Update #: 442
+##     Update #: 443
 ## URL:
 ## Doc URL:
 ## Keywords:
@@ -1166,13 +1166,14 @@ if __name__ == '__main__':
             ts = str(datetime.datetime.fromtimestamp(os.path.getmtime(os.path.abspath(logfile))).strftime("%Y%m%d_%H_%M_%S"))
             shutil.copy2(logfile,'LOGS/'+scriptname+'_'+ts+'.log')
 
-        global log              # instantiate here to make available only once
         log = setup_multiprocess_logger(log_file='stderr', logformat='%(asctime)s %(levelname)-8s %(name)-12s %(message)s', datefmt='%m-%d %H:%M')
         log = setup_multiprocess_logger(log_file=logfile, filemode='a', logformat='%(asctime)s %(levelname)-8s %(name)-12s %(message)s', datefmt='%m-%d %H:%M')
 
         log.setLevel(args.loglevel)
         log.info(logid+'Running '+scriptname+' on '+str(args.procs)+' cores.')
         log.info(logid+'CLI: '+sys.argv[0]+'{}'.format(' '.join( [shlex.quote(s) for s in sys.argv[1:]] )))
+
+        Collection.log = log
 
         preprocess(args.sequence, args.window, args.span, args.region, args.multi, args.unconstraint, args.unpaired, args.paired, args.length, args.gc, args.number, args.constrain, args.conslength, args.alphabet, args.save, args.procs, args.vrna, args.temprange, args.outdir, args.genes, args.verbosity, args.pattern, args.cutoff)
     except Exception as err:
