5a1feca011185f2aa0ed89c379b4bde145762457
domonik <schdruzzi@gmail.com>
2021-05-06 12:03:35 +0200

    visualization of sqlite db

--- RIssmed/visualize.py
+++ RIssmed/visualize.py
@@ -1,7 +1,8 @@
 import csv
 import gzip
 import sqlite3
-from typing import Dict, List
+from typing import Dict, List, Union
+import os
 
 import dash  # (version 1.12.0) pip install dash
 import dash_bootstrap_components as dbc
@@ -13,11 +14,13 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from dash.dependencies import Input, Output
 
+
+MODE = "db"
 PLOTLY_COLORS = px.colors.qualitative.Light24
 COLUMN_NAMES = {'Chr': "VARCHAR(20)",
                 'Start': "INT",
                 'End': "INT",
-                'Fold_Constraint': "VARCHAR(20)",
+                'Fold_Constraint': "VARCHAR(40)",
                 'Accessibility_difference': "REAL",
                 'Strand': "VARCHAR(2)",
                 'Distance_to_constraint': "INT",
@@ -40,16 +43,27 @@ def read_data(file: str):
 
 
 def csv_to_sqlite(file: str, db_path: str):
+    assert not os.path.exists(db_path), "database already exists."
     con = sqlite3.connect(db_path)
     cur = con.cursor()
     names = [f"{key} {COLUMN_NAMES[key]}" for key in COLUMN_NAMES]
     columns = ", ".join(names)
     call = f'CREATE TABLE IF NOT EXISTS test ( {columns} ) '
     cur.execute(call)
-    with open(file) as handle:
-        csv_reader = csv.reader(handle, delimiter="\t", )
-        for row in csv_reader:
-            cur.execute("INSERT INTO test VALUES (?,?,?,?,?,?,?,?,?,?,?,?);", row)
+    cur.execute("CREATE INDEX constraint_index ON test (Fold_Constraint)")
+    if ".gz" in file:
+        with gzip.open(file, "rt") as handle:
+            csv_reader = csv.reader(handle, delimiter="\t", )
+            x = 0
+            for row in csv_reader:
+                print(x)
+                cur.execute("INSERT INTO test VALUES (?,?,?,?,?,?,?,?,?,?,?,?);", row)
+                x += 1
+    else:
+        with open(file) as handle:
+            csv_reader = csv.reader(handle, delimiter="\t", )
+            for row in csv_reader:
+                cur.execute("INSERT INTO test VALUES (?,?,?,?,?,?,?,?,?,?,?,?);", row)
     con.commit()
     con.close()
 
@@ -94,8 +108,12 @@ def dropdown_menues(gene_id_options: List[Dict], constraint_options: List[Dict])
     return menu
 
 
-def get_app_layout(app: dash.Dash, df: pd.DataFrame):
-    sel = selectors(df)
+def get_app_layout(app: dash.Dash, df: Union[pd.DataFrame, str]):
+    if MODE == "db":
+        sel = selectors_sql(df)
+    else:
+        sel = selectors_pandas(df)
+
     app.layout = html.Div([
 
         html.Div(html.H1("RIssmed Dasboard", style={'text-align': 'center'}), className="page-header"),
@@ -108,34 +126,54 @@ def get_app_layout(app: dash.Dash, df: pd.DataFrame):
     )
 
 
-def selectors(df: pd.DataFrame):
+def selectors_pandas(df: pd.DataFrame):
     gene_id_opts = [{"label": entry, "value": entry} for entry in sorted(df["Chr"].unique())]
     constraint_opts = [{"label": entry, "value": entry} for entry in sorted(df["Fold_Constraint"].unique())]
     return gene_id_opts, constraint_opts
 
-def update_graph_via_sql(slct_chrom, slct_constraint):
-    conn = sqlite3.connect("testfile.db")
+
+def selectors_sql(db_path: str):
+    conn = sqlite3.connect(db_path)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM tasks WHERE Fold_Contraint=?", (slct_constraint))
+    cur.execute("SELECT DISTINCT Fold_Constraint FROM test")
+    constraint_opts = cur.fetchall()
+    cur.execute("SELECT DISTINCT Chr FROM test")
+    gene_id_opts = cur.fetchall()
+    conn.close()
+    gene_id_opts = [{"label": entry[0], "value": entry[0]} for entry in gene_id_opts]
+    constraint_opts = [{"label": entry[0], "value": entry[0]} for entry in constraint_opts]
+    return gene_id_opts, constraint_opts
+
+
+def update_graph_via_sql(slct_chrom, slct_constraint):
+    conn = sqlite3.connect("whole_bed.db")
+    cur = conn.cursor()
+    cur.execute("SELECT Distance_to_constraint, Accessibility_difference FROM test WHERE Fold_Constraint=? AND Chr=?",
+                (slct_constraint, slct_chrom))
     rows = cur.fetchall()
+    x, y = zip(*rows)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=y, line={"width": 4, "color": PLOTLY_COLORS[0]}))
+    fig.layout.template = "plotly_white"
+    fig.update_yaxes(range=[-1, 1])
+    conn.close()
+    return fig
+
+
 
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-csv_to_sqlite("testfile.bed", "testfile.db")
-update_graph_via_sql()
+
+#csv_to_sqlite("testfile.bed", "testfile.db")
+
 df = read_data("testfile.bed")
+
+df = "testfile.db"
+
 get_app_layout(app, df)
 
 
-@app.callback(
-    [Output(component_id='header', component_property='children'),
-     Output(component_id='plotly_graph', component_property='figure')],
-    [Input(component_id='slct_chrom', component_property='value'),
-     Input(component_id='slct_constraint', component_property='value')]
-
-)
-def update_graph(slct_chrom, slct_constraint):
-    header = f"Changes in probabilities of being unpaired (Accessibility) for Gene {slct_chrom} at constraint {slct_constraint}"
+def update_graph_via_pandas(slct_chrom, slct_constraint):
     dff = df.copy()
     dff = dff[dff["Chr"] == slct_chrom]
     fig = go.Figure()
@@ -148,6 +186,22 @@ def update_graph(slct_chrom, slct_constraint):
         fig.add_trace(go.Scatter(x=x, y=y, line={"width": 4, "color": PLOTLY_COLORS[col_idx]}))
     fig.layout.template = "plotly_white"
     fig.update_yaxes(range=[-1, 1])
+    return fig
+
+
+@app.callback(
+    [Output(component_id='header', component_property='children'),
+     Output(component_id='plotly_graph', component_property='figure')],
+    [Input(component_id='slct_chrom', component_property='value'),
+     Input(component_id='slct_constraint', component_property='value')]
+
+)
+def update_graph(slct_chrom, slct_constraint):
+    header = f"Changes in probabilities of being unpaired (Accessibility) for Gene {slct_chrom} at constraint {slct_constraint}"
+    if MODE == "db":
+        fig = update_graph_via_sql(slct_chrom, slct_constraint)
+    else:
+        fig = update_graph_via_pandas(slct_chrom, slct_constraint)
     return [header, fig]
 
 
