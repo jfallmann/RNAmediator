0e58d2928f2db31b344af0f2e2c4e82dc57b86a8
domonik <schdruzzi@gmail.com>
2021-06-18 18:23:57 +0200

    finalized bootstrap and download button

--- RIssmed/visualize.py
+++ RIssmed/visualize.py
@@ -6,9 +6,10 @@ import gzip
 import os
 import sqlite3
 import zipfile
-from tempfile import TemporaryDirectory
+from tempfile import TemporaryDirectory, NamedTemporaryFile
 from typing import List, Union
-
+import binascii
+import io
 import dash  # (version 1.12.0) pip install dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
@@ -25,7 +26,8 @@ from RNAtweaks.RIssmedArgparsers import visualiziation_parser
 
 FILEDIR = os.path.dirname(os.path.abspath(__file__))
 ASSETS_DIR = os.path.join(FILEDIR, "assets")
-app = dash.Dash("FOO", external_stylesheets=[dbc.themes.DARKLY], assets_folder=ASSETS_DIR)
+app = dash.Dash("FOO", external_stylesheets=[dbc.themes.DARKLY], assets_folder=ASSETS_DIR,
+                index_string=open(os.path.join(ASSETS_DIR, "index.html")).read())
 
 MODE = "db"
 NUMBER_OF_INTERESTING = 10
@@ -128,10 +130,13 @@ def main():
 
 
 def search_inputs():
-    columns = "p-1 col-md-3 col-sm-12 d-flex justify-content-center"
+    columns = "p-1 col-md-3 col-10 d-flex justify-content-center align-items-center text-center"
     menu = html.Div(
         [
-            html.Div([
+            html.Div(
+                html.Button("", className="prev-button", id="prev-button", n_clicks=0),
+                className="col-2 col-md-1 text-center order-2 order-md-1 m-1 m-md-0"),
+            html.Div(html.Div([
                 html.Nobr([
                     html.Label(
                         "Chr",
@@ -143,9 +148,7 @@ def search_inputs():
                               className="search-input",
                               placeholder="Search Chromosome",
                               )
-                ])
-            ], className=columns),
-            html.Div([
+                ], className=columns),
                 html.Nobr([
                     html.Label(
                         "GOI",
@@ -156,9 +159,7 @@ def search_inputs():
                               placeholder="Search Gene",
                               className="search-input",
                               )
-                ])
-            ], className=columns),
-            html.Div([
+                ], className=columns),
                 html.Nobr([
                     html.Label(
                         "Start",
@@ -166,12 +167,10 @@ def search_inputs():
                         className="search-label"
                     ),
                     dcc.Input(id="search-span-start-input",
-                              placeholder="Search Gene",
+                              placeholder="Start",
                               className="search-input",
                               )
-                ])
-            ], className=columns),
-            html.Div([
+                ], className=columns),
                 html.Nobr([
                     html.Label(
                         "End",
@@ -179,13 +178,14 @@ def search_inputs():
                         className="search-label"
                     ),
                     dcc.Input(id="search-span-end-input",
-                              placeholder="Search Gene",
+                              placeholder="End",
                               className="search-input",
                               )
-                ])
-            ], className=columns),
+                ], className=columns),
+            ], className="row justify-content-center justify-content-md-between"), className="col-12 col-md-10 order-md-2 order-1"),
+            html.Div(html.Button("", className="next-button", id="next-button", n_clicks=0), className="col-2 col-md-1 text-center order-3 order-md-3 m-1 m-md-0")
         ],
-        className="row justify-content-around align-items-center",
+        className="row justify-content-around align-items-center p-2",
 
     )
     return menu
@@ -202,9 +202,7 @@ def interesting_table(interesting: List[sqlite3.Row], prev_clicks: int = 0, next
         clickable = True
         header = interesting[0].keys()
         interesting = [(x + 1 + page * NUMBER_OF_INTERESTING, *element) for x, element in enumerate(interesting)]
-    menu = [html.Div([html.Div(html.Button("", id="prev-button", n_clicks=prev_clicks, ),
-                               style={"display": "table-cell", "margin": "auto", 'align-items': 'center',
-                                      "justify-content": "center", "vertical-align": "middle"}),
+    menu = [html.Div([
                       html.Div(html.Table(
                           [html.Tr([html.Td(html.Button("#", style={"width": "100%", "pointer-events": "None"}),
                                             className="interesting-table-header")] +
@@ -217,14 +215,9 @@ def interesting_table(interesting: List[sqlite3.Row], prev_clicks: int = 0, next
                           className="interesting-table-tablerows",
                           style={"width": "100%", "margin": "auto", 'text-align': 'center'}
 
-                      ), style={"display": "table-cell", "width": "80%"}, className="interesting-table"),
-                      html.Div(html.Button("", id="next-button", n_clicks=next_clicks,
-                                           style={"margin": "auto"}),
-                               style={"display": "table-cell", "margin": "auto", 'align-items': 'center',
-                                      "justify-content": "center", "vertical-align": "middle"})
+                      ), style={"width": "100%", "overflow": "auto"}, className="interesting-table col-12 m-0 p-0"),
                       ],
-                     style={"display": "table", "width": "100%", 'text-align': 'center', "vertical-align": "middle"},
-                     className="interesting-table-all", id="interesting-table-all")]
+                     className="col-11 justify-content-center")]
     return menu
 
 
@@ -292,16 +285,44 @@ def get_app_layout(dash_app: dash.Dash, df: Union[pd.DataFrame, str]):
 
     dash_app.layout = html.Div([
         dcc.Location(id="url", refresh=False),
-        html.Div(html.H3("RIssmed Dasboard"), className="page-header"),
 
-        html.Div([html.H4(id='header', children=[], style={"text-align": "center"}),
-                  dcc.Graph(id='plotly_graph', style={"height": "375px"})], className="databox", id="graph-box"),
-        html.Div([search_inputs()] + interesting_table(interesting),
-                 className="databox container-fluid justify-content-center", id="interesting-table-div"),
+        html.Div([
+            html.Div(html.Div(html.H3("RIssmed Dasboard"), className="databox", style={"text-align": "center"}), className="col-12 p-1 justify-content-center"),
+            html.Div([
+                html.Div([html.H4(id='header', children=[], style={"text-align": "center"}),
+                          dcc.Graph(id='plotly_graph', style={"height": "375px"}),
+                          html.Button("Download", id="download-btn"), dcc.Download(id="download-image"),
+                          html.Button("Download", type="button", id="open"),
+                          dbc.Modal([
+                              dbc.ModalHeader("Select Format"),
+                              dbc.ModalBody([
+                                  html.Div([
+                                      html.Button("SVG", id="svg-download", className="btn btn-primary col-3"),
+                                      html.Button("PNG", id="png-download", className="btn btn-primary col-3")
+                                  ], className="row justify-content-around")
+                              ]),
+                              dbc.ModalFooter(
+                                  dbc.Button(
+                                      "Close", id="close", className="ml-auto", n_clicks=0
+                                  )
+                              ),
+                          ], id="modal")
+
+
+                          ], className="databox",
+                         id="graph-box"),
+            ], className="col-12 p-1"),
+
+            html.Div([
+                html.Div([search_inputs(), html.Div(interesting_table(interesting), id="interesting-table-all", className="row justify-content-center m-1")],
+                         className="databox", id="interesting-table-div"),
+
+            ], className="col-12 p-1 justify-content-center"),
+        ], className="row justify-content-between"),
         html.Div([],
-                 className="", id="ingo"),
+                 className="row", id="ingo"),
     ],
-        id="wrapper"
+        id="wrapper", className="container-fluid"
     )
 
 
@@ -312,12 +333,12 @@ def get_ingo(first_name, name):
             img = file.read("animation_2.svg", pwd=b"ingo")
         encoded_img = base64.b64encode(img)
         svg = 'data:image/svg+xml;base64,{}'.format(encoded_img.decode())
-        ingo = html.Div(
+        ingo = html.Div(html.Div(
             [
                 html.H3("Say Hello to Ingo", style={"text-align": "center"}),
                 html.Div(html.Img(src=svg, style={"margin": "auto"}), style={"margin": "auto", "margin-top": "20px",
                                                                              "text-align": "center"})
-            ], style={"position": "relative", "height": "300"}, className="databox", id="ingo-box")
+            ], className="databox"), className="col-12 p-1 justify-content-center", id="ingo-box")
     else:
         ingo = []
     return ingo
@@ -392,6 +413,14 @@ def update_graph_via_sql(slct_chrom, slct_goi, slct_start, slct_end):
     fig.update_yaxes(range=[-1, 1], title="Probability of being unpaired")
     x_range = np.max(np.abs(distance)) if len(distance) > 0 else 120
     fig.update_xaxes(range=[-x_range, x_range], title="Distance to constraint")
+
+    fig.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ))
     conn.close()
     return fig
 
@@ -459,7 +488,9 @@ def table_click_callback(*click_args):
     [
         Output(component_id="interesting-table-all", component_property="children"),
         Output(component_id="url", component_property="pathname"),
-        Output(component_id="ingo", component_property="children")
+        Output(component_id="ingo", component_property="children"),
+        Output(component_id="prev-button", component_property="n_clicks"),
+        Output(component_id="next-button", component_property="n_clicks"),
     ],
     [
         Input(component_id="prev-button", component_property="n_clicks"),
@@ -508,7 +539,7 @@ def table_switch_callback(prev_clicks, next_clicks, inputs, search_chr_input,
     interesting = get_interesting(database, page, sorting, sorting_clicks, search_settings)
     ingo = get_ingo(search_chr_input, search_goi)
     html_table = interesting_table(interesting, prev_clicks, next_clicks, sorting, sorting_clicks=sorting_clicks)
-    return [html_table, sorting, ingo]
+    return [html_table, sorting, ingo, prev_clicks, next_clicks]
 
 
 class SearchSettings:
@@ -526,6 +557,41 @@ class SearchSettings:
 
     def __str__(self):
         return str(self.__dict__)
+
+@app.callback(
+    Output("download-image", "data"),
+    [Input("svg-download", "n_clicks"),
+     Input("png-download", "n_clicks")],
+    State(component_id='plotly_graph', component_property='figure'),
+    prevent_initial_call=True
+)
+def download_image(svg, pdf, fig):
+    trigger = callback_context.triggered[0]["prop_id"].split(".")[0]
+    format = trigger.split("-")[0]
+    fig = go.Figure(fig)
+    fig.layout.template = "plotly"
+    if format == "png":
+        with TemporaryDirectory() as handle:
+            file_name = os.path.join(handle, "foo.png")
+            fig.write_image(file_name, format="png")
+            return dcc.send_file(file_name)
+
+
+    else:
+        image = fig.to_image(format=format)
+        image = image.decode()
+        return dict(content=image, filename=f"foo.{format}")
+
+
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("open", "n_clicks"), Input("close", "n_clicks"), Input("png-download", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, n3, is_open):
+    if n1 or n2 or n3:
+        return not is_open
+    return is_open
 
 
 if __name__ == '__main__':
