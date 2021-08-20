5352c72ca25d7f2da8f599f9233a005d6d903b74
domonik <schdruzzi@gmail.com>
2021-08-20 15:51:23 +0200

    new tests for Fileprocessor

--- RIssmed/Tweaks/tests/test_Fileprocessor.py
+++ RIssmed/Tweaks/tests/test_Fileprocessor.py
@@ -2,9 +2,10 @@ import os
 from io import StringIO, TextIOWrapper
 from collections import defaultdict
 import pytest
+import gzip
 from tempfile import TemporaryDirectory
 from RIssmed.Tweaks.FileProcessor import parseseq, idfromfa, \
-    read_constraints_from_bed, make_outdir, read_paired_constraints_from_bed
+    read_constraints_from_bed, make_outdir, read_paired_constraints_from_bed, parse_annotation_bed
 
 TESTFOLDER = os.path.dirname(os.path.abspath(__file__))
 TESTDATAPATH = os.path.join(TESTFOLDER, "testdata")
@@ -18,6 +19,7 @@ TMP_TEST_DIR = TMP_DIR.name
         os.path.join(TESTDATAPATH, "test.fa"),
         "AAAAAATTTTTTGGGGG",
         os.path.join(TESTDATAPATH, "test.fa.gz"),
+        StringIO("ATATATGATATATGAATAGATATATATAGATA")
     ]
 )
 def test_parseseq(sequence):
@@ -113,3 +115,60 @@ def test_make_outdir(dir_path, caplog):
     outdir = make_outdir(dir_path)
     assert os.path.exists(outdir)
     assert caplog.text == ""
+
+
+@pytest.fixture()
+def gene_coords_string():
+    data = "chr1\t50\t100\tgene1\t.\t+\n" \
+           "chr3\t150\t200\tgene1\t.\t-"
+    return data
+
+
+@pytest.fixture()
+def gene_coords_bed(gene_coords_string):
+    file = os.path.join(TMP_TEST_DIR, "gene_coords.bed")
+    with open(file, "w") as handle:
+        handle.write(gene_coords_string)
+    yield file
+    os.remove(file)
+
+
+@pytest.fixture()
+def gene_coords_bed(gene_coords_string):
+    file = os.path.join(TMP_TEST_DIR, "gene_coords.bed")
+    with open(file, "w") as handle:
+        handle.write(gene_coords_string)
+    yield file
+    os.remove(file)
+
+@pytest.fixture()
+def gene_coords_bed_gz(gene_coords_string):
+    file = os.path.join(TMP_TEST_DIR, "gene_coords.bed.gz")
+    with gzip.open(file, "w") as handle:
+        handle.write(bytes(gene_coords_string, encoding="utf-8"))
+    yield file
+    os.remove(file)
+
+
+
+@pytest.mark.parametrize(
+    "gene_coords",
+    ["gene_coords_bed", "gene_coords_bed_gz"]
+)
+def test_parse_annotation_bed(gene_coords, request, caplog):
+    gene_coords = request.getfixturevalue(gene_coords)
+    annotations = parse_annotation_bed(gene_coords)
+    assert type(annotations) == defaultdict
+    for entry in annotations:
+        for annotation in annotations[entry]:
+            assert len(annotation.split("|")) == 2
+            coords, strand = annotation.split("|")
+            assert len(coords.split("-")) == 2
+
+
+def test_parse_annotation_bed_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        annotations = parse_annotation_bed("nonsense")
+
+
+
